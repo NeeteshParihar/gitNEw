d7bc1b495a39ede02f5cd18565e18a6c2b7bf460
NeeteshParihar <neeteshparihar22@gmail.com>
2025-02-13 02:57:45 +0530

    adding  isPrime

--- featurePerson.js
+++ featurePerson.js
@@ -9,4 +9,18 @@ console.log("hell1 Neetesh") ;
 
 
 
+function isPrime(num){
+
+    for( let i = 2 ; i  < num ; i++){
+        if( num %i == 0){
+            return false ;
+        }
+    }
+
+    return true ;
+
+}
+
+
+
 
